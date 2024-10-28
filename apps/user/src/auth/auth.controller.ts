a53adc5d95ecde9c6a1f37ab66b0ec41d3bbcf85
Abiodun <stepheng323@gmail.com>
2024-10-28 22:24:51 +0100

    add-user-ci-config-file

--- apps/user/src/auth/auth.controller.ts
+++ apps/user/src/auth/auth.controller.ts
@@ -5,7 +5,6 @@ import { AuthService } from "./auth.service";
 import { ZodValidationPipe } from '@lib/utils';
 import { CreateUserDto, createUserSchema, LoginDto, loginSchema } from '@lib/schema';
 import { convertResponse } from '@lib/helper';
-import { undefined } from 'zod';
 
 @ApiTags('Auth')
 @Controller('auth')
@@ -17,11 +16,9 @@ export class AuthController {
   @Post('signup')
   @UsePipes(new ZodValidationPipe(createUserSchema))
   async createUser(@Body() createUserDto: CreateUserDto, @Res() res: Response) {
-    console.log("got here, to the controller");
     const data =  await this.authService.signup(createUserDto)
     return convertResponse(res, data)
   }
-
 
   @Post('login')
   @UsePipes(new ZodValidationPipe(loginSchema))
@@ -31,9 +28,7 @@ export class AuthController {
   }
 
   @Get('health')
-  @UsePipes(new ZodValidationPipe(loginSchema))
   async health( @Res() res: Response) {
-    console.log("hjgfdsdsfghjkljkhgfds")
     return convertResponse(res, { data: {}, message: 'working', status: 'successful' })
   }
 }
