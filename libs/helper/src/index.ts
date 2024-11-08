c87d37b5cb2763316c031f25c8536e467849a57e
Abiodun <stepheng323@gmail.com>
2024-11-08 07:13:10 +0100

    update lib

--- libs/helper/src/index.ts
+++ libs/helper/src/index.ts
@@ -1,4 +1,4 @@
 export * from './lib/sendResponse';
-export * from './lib/response'
+export * from './lib/response';
 export * from './lib/bcrypt';
-export * from './lib/user'
+export * from './lib/user';
